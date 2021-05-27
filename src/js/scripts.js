8277a77b8b7090ac064da05af16a14e8759d2e51
thekate28 <75444145+thekate28@users.noreply.github.com>
2021-05-27 15:05:59 +0400

    Сделала слайдер на JS. Добавила картинки и их 2х версии.
    Сделан слайдер с помощью Swiper, но работает некорректно, отправляю его на проверку.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,27 +2,27 @@ let header = document.querySelector('.header');
 let headerMobile = document.querySelector('.header-mobile');
 let headerOpenBtn = document.querySelector('.menu-btn');
 let headerOverlay = document.querySelector('.header-overlay');
-let headerCloseBtn = document.querySelector ('.header-mobile__close-button');
+let headerCloseBtn = document.querySelector('.header-mobile__close-button');
 let registerModal = document.querySelector('.register-wrapper');
 let registerOpenBtn = document.querySelector('.registerBtnOpen');
 let registerMobileOpenBtn = document.querySelector('.mobileRegisterBtnOpen');
 let registerOverlay = document.querySelector('.register-overlay');
-let registerCloseBtn = document.querySelector ('.register-wrapper__close-btn');
+let registerCloseBtn = document.querySelector('.register-wrapper__close-btn');
 let signOpenBtn = document.querySelector('.signBtnOpen');
 let signMobileOpenBtn = document.querySelector('.mobileSignBtnOpen');
 let signModal = document.querySelector('.sign-wrapper');
 let signOverlay = document.querySelector('.sign-overlay');
-let signCloseBtn = document.querySelector ('.sign-wrapper__close-btn');
+let signCloseBtn = document.querySelector('.sign-wrapper__close-btn');
 let messageOpenBtn = document.querySelector('.modalOpenBtn');
 let messageModal = document.querySelector('.message-wrapper');
 let messageOverlay = document.querySelector('.message-overlay');
-let messageCloseBtn = document.querySelector ('.message-wrapper__close-btn');
+let messageCloseBtn = document.querySelector('.message-wrapper__close-btn');
 
 function headerOpen() {
     header.classList.add('header-none');
     headerMobile.classList.add('open');
     headerOverlay.classList.add('open');
-    window.addEventListener('keydown', function(evt){
+    window.addEventListener('keydown', function (evt) {
         if (evt.code === "Escape") {
             header.classList.remove('header-none');
             headerMobile.classList.remove('open');
@@ -40,7 +40,7 @@ function registerOpen() {
     registerOverlay.classList.add('open');
     headerMobile.classList.remove('open');
     headerOverlay.classList.remove('open');
-    window.addEventListener('keydown', function(evt){
+    window.addEventListener('keydown', function (evt) {
         if (evt.code === "Escape") {
             registerModal.classList.remove('open');
             registerOverlay.classList.remove('open');
@@ -56,7 +56,7 @@ function signOpen() {
     signOverlay.classList.add('open');
     headerMobile.classList.remove('open');
     headerOverlay.classList.remove('open');
-    window.addEventListener('keydown', function(evt){
+    window.addEventListener('keydown', function (evt) {
         if (evt.code === "Escape") {
             signModal.classList.remove('open');
             signOverlay.classList.remove('open');
@@ -70,7 +70,7 @@ function signClose() {
 function messageOpen() {
     messageModal.classList.add('open');
     messageOverlay.classList.add('open');
-    window.addEventListener('keydown', function(evt){
+    window.addEventListener('keydown', function (evt) {
         if (evt.code === "Escape") {
             messageModal.classList.remove('open');
             messageOverlay.classList.remove('open');
@@ -80,32 +80,55 @@ function messageOpen() {
 function messageClose() {
     messageModal.classList.remove('open');
     messageOverlay.classList.remove('open');
-} 
-headerOpenBtn.addEventListener ('click', function(){
-    headerOpen() 
+}
+headerOpenBtn.addEventListener('click', function () {
+    headerOpen()
 })
-headerCloseBtn.addEventListener('click', function(){
+headerCloseBtn.addEventListener('click', function () {
     headerClose()
 })
-registerOpenBtn.addEventListener ('click', function(){
+registerOpenBtn.addEventListener('click', function () {
     registerOpen()
 })
-registerMobileOpenBtn.addEventListener ('click', function(){
+registerMobileOpenBtn.addEventListener('click', function () {
     registerOpen()
 })
-registerCloseBtn.addEventListener('click', function(){
+registerCloseBtn.addEventListener('click', function () {
     registerClose()
 })
 
-signOpenBtn.addEventListener ('click', function(){
-    signOpen() 
+signOpenBtn.addEventListener('click', function () {
+    signOpen()
 })
-signCloseBtn.addEventListener('click', function(){
+signCloseBtn.addEventListener('click', function () {
     signClose()
 })
-messageOpenBtn.addEventListener ('click', function(){
-    messageOpen() 
+messageOpenBtn.addEventListener('click', function () {
+    messageOpen()
 })
-messageCloseBtn.addEventListener('click', function(){
+messageCloseBtn.addEventListener('click', function () {
     messageClose()
 })
+
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 2,
+  
+    breakpoints: {
+      720: {
+        slidesPerView: 1,
+      },
+    }
+  });
